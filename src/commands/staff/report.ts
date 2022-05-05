adf7c079d62b7c1db51ce1fc8d0dccda861fdfbb
Wesley Lins <wesleylins.soares@gmail.com>
2022-05-05 11:03:31 -0300

    chore: permission to export csv

--- src/commands/staff/report.ts
+++ src/commands/staff/report.ts
@@ -2,6 +2,7 @@ import { Command } from "../../structures/Command";
 import { fetchToCSV } from "../../utils/db";
 import fs from "fs";
 import { MessageEmbed } from "discord.js";
+import { embedPermission } from "../../utils/embeds";
 
 export default new Command({
   name: "relatorio",
@@ -39,14 +40,20 @@ export default new Command({
       .setTitle("Relatório")
       .setDescription(`Relatório exportado com sucesso!`);
 
-    interaction.editReply({
-      embeds: [embedExport],
-    });
+    if (interaction.memberPermissions.has("ADMINISTRATOR")) {
+      interaction.editReply({
+        embeds: [embedExport],
+      });
 
-    fs.writeFile("data.csv", data, (err) => {
-      if (err) throw err;
+      fs.writeFile("data.csv", data, (err) => {
+        if (err) throw err;
 
-      interaction.user.send({ files: ["data.csv"] });
-    });
+        interaction.user.send({ files: ["data.csv"] });
+      });
+    } else {
+      interaction.editReply({
+        embeds: [embedPermission],
+      });
+    }
   },
 });
