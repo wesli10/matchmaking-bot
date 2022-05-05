5fc8aaf4cf38ab54cc78ca6cfbac3fb1401fd575
Pierre Ortega <pierreandreisortega@gmail.com>
2022-05-05 12:20:41 -0700

    adds log

--- src/index.ts
+++ src/index.ts
@@ -5,3 +5,9 @@ dotenv.config();
 export const client = new ExtendedClient();
 
 client.start();
+
+client.on("debug", (...message) => console.log("debug", ...message));
+client.on("warn", (...message) => console.warn("warn", ...message));
+client.on("error", (...message) => console.error("error", ...message));
+client.on("shardError", (...message) => console.log("shardError", ...message));
+client.on("log", (...message) => console.log("log", ...message));
