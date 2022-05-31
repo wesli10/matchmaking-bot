2cc9d941133f627751e0d40237f4b539be53b62e
Wesley Lins <wesleylins.soares@gmail.com>
2022-05-31 15:46:50 -0300

    chore: logs to track cancel match error

--- src/commands/staff/startLobby.ts
+++ src/commands/staff/startLobby.ts
@@ -78,22 +78,22 @@ export default new Command({
         id: interaction.guild.id,
         deny: ["VIEW_CHANNEL"],
       },
-      {
-        id: role_aux_event,
-        allow: ["VIEW_CHANNEL"],
-      },
-      {
-        id: role_event,
-        allow: ["VIEW_CHANNEL"],
-      },
-      {
-        id: role_moderator,
-        allow: ["VIEW_CHANNEL"],
-      },
       // {
-      //   id: role_admin,
-      //   allow: ["VIEW_CHANNEL"],
+      //   id: role_aux_event,
+      //   allow: ["VIEW_CHANNEL", "MANAGE_CHANNELS"],
       // },
+      // {
+      //   id: role_event,
+      //   allow: ["VIEW_CHANNEL", "MANAGE_CHANNELS"],
+      // },
+      // {
+      //   id: role_moderator,
+      //   allow: ["VIEW_CHANNEL", "MANAGE_CHANNELS"],
+      // },
+      {
+        id: role_admin,
+        allow: ["VIEW_CHANNEL", "MANAGE_CHANNELS"],
+      },
       // Add players permissions as well
       ...players.map((player) => {
         return {
@@ -109,22 +109,22 @@ export default new Command({
         id: interaction.guild.id,
         deny: ["VIEW_CHANNEL"],
       },
-      {
-        id: role_aux_event,
-        allow: ["VIEW_CHANNEL"],
-      },
-      {
-        id: role_event,
-        allow: ["VIEW_CHANNEL"],
-      },
-      {
-        id: role_moderator,
-        allow: ["VIEW_CHANNEL"],
-      },
       // {
-      //   id: role_admin,
-      //   allow: ["VIEW_CHANNEL"],
+      //   id: role_aux_event,
+      //   allow: ["VIEW_CHANNEL", "MANAGE_CHANNELS"],
       // },
+      // {
+      //   id: role_event,
+      //   allow: ["VIEW_CHANNEL", "MANAGE_CHANNELS"],
+      // },
+      // {
+      //   id: role_moderator,
+      //   allow: ["VIEW_CHANNEL", "MANAGE_CHANNELS"],
+      // },
+      {
+        id: role_admin,
+        allow: ["VIEW_CHANNEL", "MANAGE_CHANNELS"],
+      },
       // Add players permissions as well
       ...players.map((player) => {
         return {
@@ -170,9 +170,16 @@ export default new Command({
 
     for (const player of players) {
       const member = await interaction.guild.members.fetch(player.user_id);
+      const playerTeam = player.team === 1 ? "Time 1" : "Time 2";
       await Promise.all([
         updateInMatch("users_4v4", player.user_id, true),
-        updateUserTeam(player.user_id, player.team === 1 ? "Time1" : "Time 2"),
+        create4v4Lobby(
+          player.user_id,
+          category.id,
+          interaction.user.id,
+          playerTeam
+        ),
+        updateUserTeam(player.user_id, player.team === 1 ? "Time 1" : "Time 2"),
         updateCategory(player.user_id, category.id),
         updateModerator(player.user_id, interaction.user.id),
         member.voice
@@ -181,11 +188,6 @@ export default new Command({
       ]);
     }
 
-    await create4v4Lobby({
-      players,
-      category_id: category.id,
-      moderator_id: interaction.user.id,
-    });
     await textChatAnnouncements.send({
       content: `Time 1: <@${players
         .filter((player) => player.team === 1)
@@ -389,14 +391,16 @@ export async function handleButtonInteractionPlayerMenu(
           } else if (reaction.emoji.name === "❌" && !user.bot) {
             const member = await btnInt.guild.members.fetch(user.id);
             console.log("cancel button is pressed!");
-            if (
-              reaction.count === 2 &&
-              member.permissions.has("MODERATE_MEMBERS")
-            ) {
+
+            if (member.permissions.has("MODERATE_MEMBERS")) {
+              console.log(member.permissions.has("MODERATE_MEMBERS"));
+              winnerTeam = "Cancelado";
               try {
-                await btnInt.channel.send({
+                const message = await btnInt.channel.send({
                   embeds: [PartidaCancelada],
                 });
+                console.log(message);
+                console.log("passei do await");
                 collectorReaction.stop();
               } catch (error) {
                 console.log(error);
