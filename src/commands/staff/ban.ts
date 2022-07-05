5ba1bdf6572ca19142281f58f89333877b82911f
Matheus <matheusluciox@gmail.com>
2022-07-05 14:07:41 -0300

    chore: /banir command - ban user for an amount of hours

---
+++ src/commands/staff/ban.ts
@@ -0,0 +1,83 @@
+import { MessageEmbed, TextChannel } from "discord.js";
+import { DISCORD_CONFIG } from "../../configs/discord.config";
+import { Command } from "../../structures/Command";
+import { banUser, isBanned } from "../../utils/db";
+
+export default new Command({
+  name: "banir",
+  description: "Banir um usuario do servidor por determinadas horas.",
+  userPermissions: ["ADMINISTRATOR"],
+  options: [
+    { name: "user", description: "Usuario", type: "USER", required: true },
+    {
+      name: "hours",
+      description: "Quantidade de horas",
+      type: "NUMBER",
+      required: true,
+    },
+    { name: "reason", description: "Motivo", type: "STRING", required: true },
+  ],
+  run: async ({ interaction }) => {
+    const role1 = DISCORD_CONFIG.roles.moderator;
+    const role2 = DISCORD_CONFIG.roles.event;
+    const role3 = DISCORD_CONFIG.roles.aux_event;
+    const roleTeste = DISCORD_CONFIG.roles.admin;
+    const admin = JSON.stringify(interaction.member.roles.valueOf());
+
+    if (
+      admin.includes(role1) ||
+      admin.includes(role2) ||
+      admin.includes(role3) ||
+      admin.includes(roleTeste)
+    ) {
+      const user = interaction.options.getUser("user");
+      const hours = interaction.options.getNumber("hours");
+      const reason = interaction.options.getString("reason");
+
+      const banned = await isBanned(user.id);
+
+      if (banned) {
+        interaction
+          .editReply({
+            embeds: [
+              new MessageEmbed()
+                .setColor("#ff0000")
+                .setTitle("Usuario já está banido!")
+                .setDescription(`O usuario ${user.tag} já está banido!`),
+            ],
+          })
+          .then(() => setTimeout(() => interaction.deleteReply(), 3000));
+
+        return;
+      }
+
+      try {
+        const end_date = new Date();
+        end_date.setHours(end_date.getHours() + hours);
+
+        await banUser(user.id, end_date, reason, interaction.user.id);
+
+        const kick_embed = new MessageEmbed()
+          .setColor("#fd4a5f")
+          .setTitle("Banido!")
+          .setDescription(
+            `Usuario: ${user.tag} foi banido por ${hours} hora(s)!`
+          );
+
+        const channel = interaction.guild.channels.cache.get(
+          interaction.channelId
+        ) as TextChannel;
+
+        const interactionMessage = await channel.send({
+          embeds: [kick_embed],
+        });
+
+        setTimeout(() => interactionMessage.delete(), 3000);
+
+        await interaction.deleteReply();
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  },
+});
